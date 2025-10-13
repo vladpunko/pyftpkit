4b0aa93eb3e2003492792f7496370658a75e14c3
Vladislav Punko <iam.vlad.punko@gmail.com>
2025-10-13 21:34:12 +0200

    Add base exception class

---
+++ src/pyftpkit/exceptions.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2025 (c) Vladislav Punko <iam.vlad.punko@gmail.com>
+
+__all__ = ["FTPError"]
+
+
+class FTPError(Exception):
+    """Base exception for all errors related to FTP operations."""
