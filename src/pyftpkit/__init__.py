55b41a833af3bd9960eadffe12cabcd107bed63c
Vladislav Punko <iam.vlad.punko@gmail.com>
2025-10-26 13:38:23 +0100

    Migrate logger configuration

--- src/pyftpkit/__init__.py
+++ src/pyftpkit/__init__.py
@@ -2,49 +2,6 @@
 
 # Copyright 2025 (c) Vladislav Punko <iam.vlad.punko@gmail.com>
 
-import logging
-import logging.config
-import os
-import typing
+from pyftpkit import logger_wrapper
 
-logging.config.dictConfig(
-    {
-        "disable_existing_loggers": False,
-        "formatters": {
-            "default": {
-                "format": "%(asctime)s - %(levelname)s :: %(name)s :: %(message)s",
-            },
-        },
-        "handlers": {
-            "stderr": {
-                "class": "logging.StreamHandler",
-                "formatter": "default",
-                "stream": "ext://sys.stderr",
-            },
-            "logfile": {
-                "class": "logging.FileHandler",
-                "formatter": "default",
-                "filename": os.environ.get("PYFTPKIT_LOGGER_PATH", "/tmp/pyftpkit.log"),
-                "mode": "at",
-                "encoding": "utf-8",
-            },
-        },
-        "loggers": {
-            "pyftpkit": {
-                "handlers": ["stderr", "logfile"],
-                "level": typing.cast(
-                    int,
-                    {
-                        "CRITICAL": logging.CRITICAL,
-                        "ERROR": logging.ERROR,
-                        "WARNING": logging.WARNING,
-                        "INFO": logging.INFO,
-                        "DEBUG": logging.DEBUG,
-                        "NOTSET": logging.NOTSET,
-                    }.get(os.environ.get("PYFTPKIT_LOGGER_LEVEL", "warning").upper()),
-                ),
-            },
-        },
-        "version": 1,
-    }
-)
+logger_wrapper.setup()
---
+++ src/pyftpkit/logger_wrapper.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2025 (c) Vladislav Punko <iam.vlad.punko@gmail.com>
+
+import logging
+import logging.config
+import os
+import pathlib
+import typing
+
+__all__ = ["setup"]
+
+
+def setup(level: str = "INFO", path: str | pathlib.Path = "/tmp/pyftpkit.log") -> None:
+    """Sets up the logging system for the packagae."""
+    logging.config.dictConfig(
+        {
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "format": "%(asctime)s - %(levelname)s :: %(name)s :: %(message)s",
+                },
+            },
+            "handlers": {
+                "file": {
+                    "class": "logging.FileHandler",
+                    "formatter": "default",
+                    "filename": os.environ.get("PYFTPKIT_LOGGER_PATH", str(path)),
+                    "mode": "at",
+                    "encoding": "utf-8",
+                },
+                "stderr": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                    "stream": "ext://sys.stderr",
+                },
+            },
+            "loggers": {
+                "pyftpkit": {
+                    "handlers": ["file", "stderr"],
+                    "level": typing.cast(
+                        int,
+                        {
+                            "CRITICAL": logging.CRITICAL,
+                            "ERROR": logging.ERROR,
+                            "WARNING": logging.WARNING,
+                            "INFO": logging.INFO,
+                            "DEBUG": logging.DEBUG,
+                            "NOTSET": logging.NOTSET,
+                        }.get(os.environ.get("PYFTPKIT_LOGGER_LEVEL", level).upper()),
+                    ),
+                },
+            },
+            "version": 1,
+        }
+    )
